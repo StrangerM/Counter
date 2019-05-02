27224f92d4c06940197a1927c6988a3e81c82a53
plyskay@mail.ru <yuriyply@gmail.com>
2019-05-02 18:51:10 +0300

    saved

---
+++ Coun/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Plyskay
+ * Date: 9/5/2018
+ * Time: 10:00 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Coun
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(178, 207);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(60, 14);
+			this.label1.TabIndex = 0;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 90000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.ForeColor = System.Drawing.Color.Navy;
+			this.label2.Location = new System.Drawing.Point(58, 19);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(151, 35);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "EOL_16";
+			this.label2.Click += new System.EventHandler(this.Label2Click);
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.ForeColor = System.Drawing.Color.MediumSeaGreen;
+			this.label3.Location = new System.Drawing.Point(12, 76);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "PASS";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.ForeColor = System.Drawing.Color.Red;
+			this.label4.Location = new System.Drawing.Point(12, 120);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "FAIL";
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.label5.Location = new System.Drawing.Point(98, 76);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(58, 23);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "label5";
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.ForeColor = System.Drawing.Color.Red;
+			this.label6.Location = new System.Drawing.Point(98, 120);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(58, 23);
+			this.label6.TabIndex = 5;
+			this.label6.Text = "label6";
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.label7.Location = new System.Drawing.Point(98, 184);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(58, 23);
+			this.label7.TabIndex = 6;
+			this.label7.Text = "label7";
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+			this.label8.Location = new System.Drawing.Point(12, 184);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(80, 23);
+			this.label8.TabIndex = 7;
+			this.label8.Text = "Plan";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(232, 222);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "MainForm";
+			this.Text = "Coun";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Coun/MainForm.cs
@@ -0,0 +1,156 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Plyskay
+ * Date: 9/5/2018
+ * Time: 10:00 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace Coun
+{
+	public partial class MainForm : Form
+	{
+	  //const	 int plan = 1725;
+	  //const int d = 432;
+	  TcpClient clientSocket = new TcpClient();
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+			
+		
+			    int y;
+			    int m = 432;
+				int z =Co(out y, out m);
+				label5.Text=z.ToString();
+				label6.Text=y.ToString();
+              // label1.Text = DateTime.Now.ToString("h:mm tt");
+               label7.Text=m.ToString();
+             
+			
+		}
+		
+		public int Co(out int y, out int m)
+		{
+			   var d1 = DateTime.Now;
+			   DateTime d2 = new DateTime();
+				int x = 0;
+				 y = 0;
+				 m = 432;
+			try{
+				//Thread.Sleep(60000);	
+				//string[] filePaths = Directory.GetFiles(@"L:\DATA\Aero3_EOL\reports","*.txt");
+				string[] filePaths = Directory.GetFiles(@"C:\Users\plyskay\Desktop\11","*.txt");
+				
+				foreach(var st in filePaths)
+				{
+				 string filebody = File.ReadAllText(st);
+				 d2 = File.GetLastWriteTime(st);
+				 if((filebody.Contains("UUT Result: Passed"))&((d1.DayOfYear == d2.DayOfYear)||((int)d1.DayOfYear-(int)d2.DayOfYear==1 )))
+				{
+				 	if((d2.Hour >=6)&&((float)d2.Hour<=18.3)&&(d1.DayOfYear == d2.DayOfYear)){
+				 		x++; m--;}
+				 	else if(((float)d2.Hour >18.3)&&(d2.Hour<=23)&&(d1.DayOfYear == d2.DayOfYear)){
+				 		x++; m--;
+				 	}else if(((d2.Hour >=0) && (d2.Hour <=6)&& (d1.DayOfYear == d2.DayOfYear))){
+				 		x++; m--;
+				 	}
+				 	
+				}
+				 if((filebody.Contains("UUT Result: Failed"))&&((d1.DayOfYear == d2.DayOfYear)||((int)d1.DayOfYear-(int)d2.DayOfYear==1 )))
+				 {
+				 	if((d2.Hour >=6)&((float)d2.Hour<=18.3)&(d1.DayOfYear == d2.DayOfYear)){
+				 		y++;}
+				 	else if(((float)d2.Hour >18.3)&(d2.Hour<=23)&(d1.DayOfYear == d2.DayOfYear)){
+				 		y++;
+				 	}else if(((d2.Hour >=0) & (d2.Hour <=6)& (d1.DayOfYear == d2.DayOfYear))){
+				 		y++;
+				   }
+				}
+				
+				}			
+			}
+			catch(Exception e)
+			{
+				MessageBox.Show(e.ToString());
+			}
+			if(((d1.Hour== 6)&&(d1.Minute <2))|((d1.Hour == 18) &&(d1.Minute < 32) ))
+			   {
+			    
+			   	x = 0;
+			   	y = 0;
+			   	m = 432;
+			   }
+		//C:\Users\plyskay\Documents
+		//C:\TEMP\16\Data
+		try{
+			File.WriteAllLines(@"C:\Users\plyskay\Documents\Data.txt", new string [] {x.ToString(),y.ToString()});
+			}
+		catch(Exception e){
+			MessageBox.Show(e.ToString());
+		}
+			return x;		
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			//TimeSpan sp;
+			//string d = DateTime.Now.ToString("h:mm tt");
+			//label1.Text = d;
+			//var s1= DateTime.Now;
+		    //Thread.Sleep(1000);
+			//var s2 = DateTime.Now.AddSeconds(10.0d);
+			
+			//sp = s2-s1;
+			//double r = sp.TotalSeconds;
+		    //if(r>=10)
+			
+				int y;
+				int m = 432;
+				int z = Co(out y, out m);
+				label5.Text=z.ToString();
+				label6.Text=y.ToString();
+				label7.Text=m.ToString();
+				string data = z + ","+ y;
+			NetworkStream serverStream = clientSocket.GetStream();
+			byte[] outStream = System.Text.Encoding.ASCII.GetBytes(data + "$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+//            byte[] inStream = new byte[10025];
+//            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+//            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+		    //	}
+	
+		}
+	
+		void Label2Click(object sender, EventArgs e)
+		{
+	
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			try{
+				clientSocket.Connect("10.73.88.147", 8888);}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			
+			}
+		
+		}
+			
+			//Refresh();
+		}
+		
+		
+	}
+
